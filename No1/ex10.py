d1f7857a2ad5b2a6804b2c26acaead017742e3ce
s_10018 <shiyuuzi99@gmail.com>
2014-05-13 00:58:11 +0900

    少し修正

--- No1/ex10.py
+++ No1/ex10.py
@@ -7,8 +7,12 @@ for line in sys.stdin:
     line = line.rstrip()
     counter[line] = counter[line] + 1
 
-for key, value in sorted(counter.items(), key=lambda x: x[1], reverse=True):
-    print "{:4d} {}".format(value, key)
+try:
+    for k, v in sorted(counter.items(), key=lambda x: x[1], reverse=True):
+        print("{:4d} {}".format(v, k))
+        sys.stdout.flush()
+except:
+    exit(1)
 
 ''' 確認の仕方
 python ex10.py < col2.txt | sort > hist1.txt
@@ -16,4 +20,3 @@ sort col2.txt | uniq -c | sort -k 2,1 -n -r > hist2.txt
 diff hist1.txt hist2.txt
 (同じ頻度のソートでうまくいかない可能性あり)
 '''
-
