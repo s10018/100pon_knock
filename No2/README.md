30d0dbcf486a7f9773b6e897302585ad064aa685
Mai Omura <shiyuuzi99@gmail.com>
2015-02-16 23:07:52 +0900

    some fix

--- No2/README.md
+++ No2/README.md
@@ -1,6 +1,14 @@
+# TODO: (20)
+
+なんとなくやるせないので
+http://apps.timwhitlock.info/emoji/tables/unicode
+見ながら書き直す
+
 # (20)の解釈
 
-絵文字は顔文字として書いてます....
+絵文字は顔文字として書いてます....`(´・ω・｀)`やなど
+http://www.chokkan.org/publication/coding-for-researchers.pdf
+を見る限り絵文字は絵文字？っぽいですね
 
 # tweet.txt.gzの場所
 
--- No2/ex11.py
+++ No2/ex11.py
@@ -2,6 +2,7 @@
 import gzip
 import pit
 
+
 root = pit.Pit().get("100pon")["path"]
 
 with gzip.open(root + "/tweets.txt.gz") as tweets:
@@ -9,6 +10,7 @@ with gzip.open(root + "/tweets.txt.gz") as tweets:
         tweet = unicode(tweet.rstrip(), "UTF-8")
         if u"拡散希望" in tweet:
             print tweet.encode("UTF-8")
+
 
 '''
 拡散希望含むツイートをそのまま1行出力します．
